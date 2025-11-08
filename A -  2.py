9dc205d794dfad46e74b1b9249c5aab4316908b5
Samarsh7520 <samarsh.jaiz@gmail.com>
2025-11-08 17:25:46 +0530

    Add files via upload

---
+++ A -  2.py
@@ -0,0 +1,5 @@
+n1 = int(input("Enter a Number:"))
+if n1 % 2 == 0:
+    print("The number is Even")
+elif n1 % 2 != 0:
+    print("The number is Odd")
---
+++ A - 1.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter the first Number: "))
+n2 = int(input("Enter the second Number: "))
+n3 = int(input("Enter the third Number: "))
+if n1>n2 and n1>n3:
+    print("N1 is the largest number")
+elif n1>n2 or n1>n3:
+    print("N1 is the second largest Number")
+
